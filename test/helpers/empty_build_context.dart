802fb1ee866c42cac11cbc3668605f86f7e03f5d
iosemagno <iose.magno@gmail.com>
2023-06-20 16:22:58 +0200

    Upgrade to make it compile with latest flutter master

--- test/helpers/empty_build_context.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/widgets.dart';
-
-class EmptyBuildContext implements BuildContext {
-  @override
-  dynamic noSuchMethod(Invocation invocation) {
-    throw UnimplementedError();
-  }
-}
